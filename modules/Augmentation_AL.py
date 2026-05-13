5dede04d34dc42240491fe78a199603cf8788c23
Garfield-1314 <447972538@qq.com>
2026-05-13 17:17:12 +0800

    fix exe bug

--- modules/Augmentation_AL.py
+++ modules/Augmentation_AL.py
@@ -1,5 +1,6 @@
 ﻿import os
 import cv2
+import numpy as np
 from albumentations import (
     Compose, HorizontalFlip, Rotate,
     RGBShift, RandomBrightnessContrast,MotionBlur,VerticalFlip,HueSaturationValue,ElasticTransform,OpticalDistortion
